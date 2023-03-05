35b78b6e6cc58fe6693a2517a603c9ad2e4cba77
Aloys Berger <alo@reaktive.com>
2023-03-06 00:57:35 +1000

    feat: Add daisy UI

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,10 @@
 export default function Index() {
   return (
-    <h1 className="text-3xl font-bold underline text-indigo-500">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold text-indigo-500 underline">
+        Hello world!
+      </h1>
+      <button className="btn">Button</button>
+    </>
+  );
 }
