b6aa6bdc360ad9f73e03eda70ce04e4ae82390a2
Aloys Berger <alo@reaktive.com>
2023-03-20 06:47:33 +1000

    style: Minor style improvements on the add a spot form

--- app/components/AddSpotForm.tsx
+++ app/components/AddSpotForm.tsx
@@ -15,78 +15,82 @@ export enum FormFields {
   WindDirections = "windDirections",
 }
 const windDirections = [
-  "north",
-  "south",
-  "east",
-  "west",
-  "north-east",
   "north-west",
-  "south-east",
+  "north",
+  "north-east",
+  "west",
+  "east",
   "south-west",
+  "south",
+  "south-east",
 ];
 
 export function AddSpotForm({ errors, values }: Props) {
   return (
-    <div>
+    <div className="flex flex-col py-12">
       <h1 className="heading">Add a spot</h1>
+      <h2>General</h2>
       <div className="w-full form-control">
         <div>
           <label className="label" htmlFor="name">
-            <span className="label-text">name</span>
+            <span className="label-text">Name</span>
           </label>
           <input
             name={FormFields.Name}
             id={FormFields.Name}
             type="text"
             defaultValue={values?.name}
-            className="w-full max-w-xs input input-bordered"
+            className="w-full input input-bordered"
           />
           {errors?.name && <p className={"text-error"}>{errors.name}</p>}
         </div>
-        <div className="w-full max-w-xs form-control">
+        <div className="w-full form-control">
           <label className="label" htmlFor="description">
-            <span className="label-text">description</span>
+            <span className="label-text">Description</span>
           </label>
           <input
             type="text"
             name="description"
             id="description"
-            className="w-full max-w-xs input input-bordered"
+            className="w-full input input-bordered"
             defaultValue={values?.description}
           />
           {errors?.description && (
             <p className={"text-error"}>{errors.description}</p>
           )}
         </div>
-        <div className="w-full max-w-xs form-control">
+        <h2>Location</h2>
+        <div className="w-full form-control">
           <label className="label" htmlFor="latitude">
             <span className="label-text">Latitude</span>
           </label>
           <input
             name="latitude"
             id="latitude"
-            className="w-full max-w-xs input input-bordered"
+            className="w-full input input-bordered"
             defaultValue={values?.latitude}
           />
           {errors?.latitude && (
             <p className={"text-error"}>{errors.latitude}</p>
           )}
         </div>
-        <div className="w-full max-w-xs form-control">
+        <div className="w-full form-control">
           <label className="label" htmlFor="longitude">
             <span className="label-text">Longitude</span>
           </label>
           <input
             name="longitude"
             id="longitude"
-            className="w-full max-w-xs input input-bordered"
+            className="w-full input input-bordered"
             defaultValue={values?.longitude}
           />
           {errors?.longitude && (
             <p className={"text-error"}>{errors.longitude}</p>
           )}
         </div>
-        <div className="w-full max-w-xs form-control">
+        <h2>Wind</h2>
+        <h3>Strength</h3>
+        <div className="w-full form-control">
           <label className="label" htmlFor="wind-min">
             <span className="label-text">Min wind</span>
           </label>
@@ -96,10 +100,10 @@ export function AddSpotForm({ errors, values }: Props) {
             id="minWind"
             type="number"
             defaultValue={values?.windRange?.at(0)}
-            className="w-full max-w-xs input input-bordered"
+            className="w-full input input-bordered"
           />
         </div>
-        <div className="w-full max-w-xs form-control">
+        <div className="w-full form-control">
           <label className="label" htmlFor="wind-max">
             <span className="label-text">Max wind</span>
           </label>
@@ -109,37 +113,49 @@ export function AddSpotForm({ errors, values }: Props) {
             aria-label="maxWind"
             type="number"
             defaultValue={values?.windRange?.at(-1)}
-            className="w-full max-w-xs input input-bordered"
+            className="w-full input input-bordered"
           />
           {errors?.windRange && (
             <p className={"text-error"}>{errors.windRange}</p>
           )}
         </div>
-        <div className="flex flex-wrap">
-          {windDirections.map((direction) => (
-            <div className="w-full max-w-xs form-control" key={direction}>
-              <label className="label" htmlFor={`windDirections-${direction}`}>
-                <span className="label-text">{direction}</span>
-              </label>
-              <input
-                name="windDirections"
-                aria-label={`windDirections-${direction}`}
-                id={`windDirections-${direction}`}
-                value={direction}
-                defaultChecked={values?.windDirections?.includes(direction)}
-                type="checkbox"
-                className="w-full max-w-xs input input-bordered"
-              />
-            </div>
+        <h3>Directions</h3>
+        <div className="grid grid-cols-3">
+          {windDirections.map((direction, index) => (
+            <>
+              {index === 4 && <div></div>}
+              <div className="items-center form-control" key={direction}>
+                <label
+                  className="cursor-pointer label"
+                  htmlFor={`windDirections-${direction}`}
+                >
+                  <span className="capitalize label-text">{direction}</span>
+                </label>
+                <input
+                  name="windDirections"
+                  aria-label={`windDirections-${direction}`}
+                  id={`windDirections-${direction}`}
+                  value={direction}
+                  defaultChecked={values?.windDirections?.includes(direction)}
+                  type="checkbox"
+                  className="self-center cursor-pointer checkbox checkbox-primary"
+                />
+              </div>
+            </>
           ))}
           {errors?.windDirections && (
             <p className={"text-error"}>{errors.windDirections}</p>
           )}
         </div>
       </div>
-      <button type="submit" className="btn">
-        Add
-      </button>
+      <div className="flex flex-col my-12 gap-y-4">
+        <button type="submit" className="btn btn-primary">
+          Add
+        </button>
+        <button type="submit" className="btn btn-outline btn-secondary">
+          cancel
+        </button>
+      </div>
     </div>
   );
 }
